2743c5ffe3274d81a62e82f1d202d490d442ad22
Enrique Cruz <enrique_magdalenocruz@hotmail.com>
2020-11-28 17:49:27 -0600

    Se agrega anotación @RibbonClient en clase de de configuración y modifica la clase ProductoClienteRest para omitir la URL y dejar que maneje con Ribbon

--- src/main/java/com/formacionbdi/springboot/app/item/clientes/ProductoClienteRest.java
+++ src/main/java/com/formacionbdi/springboot/app/item/clientes/ProductoClienteRest.java
@@ -11,7 +11,7 @@ import com.formacionbdi.springboot.app.item.models.Producto;
 
 
 //La clase al estar anotada con FeignClient , es elegible para ser inyectada
-@FeignClient(name="servicio-productos",url="localhost:8001")
+@FeignClient(name="servicio-productos")
 public interface ProductoClienteRest {
 	
 	@RequestMapping(value = "/producto", method = RequestMethod.GET)
